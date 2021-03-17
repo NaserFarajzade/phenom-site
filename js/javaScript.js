7ca5947edca17893f14344f165bf37ba48b43bef
naser farajzade <13781378.nf@gmail.com>
2021-03-17 05:40:16 -0500

    navigation var changed

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,21 +1,42 @@
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("navbar").style.padding = "0px 50px";
+//     document.getElementById("logo").style.fontSize = "25px";
+//     document.getElementById("navbar").style.backgroundColor="rgb(21 3 46)";
+//     document.getElementById("logo-navbar").style.width="15%";
+//     // document.getElementById("navbar").style.background="linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(7,7,8,1) 53%, rgba(0,0,0,0.9452731776304272) 100%)";
+
+//   } else {
+//     document.getElementById("navbar").style.padding = "50px 50px";
+//     document.getElementById("logo").style.fontSize = "35px";
+//     document.getElementById("navbar").style.backgroundColor="#f1f1f100";
+
+//   }
+// }
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "0px 50px";
-    document.getElementById("logo").style.fontSize = "25px";
-    document.getElementById("navbar").style.backgroundColor="rgb(21 3 46)";
-    document.getElementById("logo-navbar").style.width="15%";
+    // document.getElementById("mainNav").style.padding = "20px 20px";
+    // document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("mainNav").style.backgroundColor="#010613";
+    document.getElementById("logo-navbar").style.width=150;
     // document.getElementById("navbar").style.background="linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(7,7,8,1) 53%, rgba(0,0,0,0.9452731776304272) 100%)";
 
   } else {
-    document.getElementById("navbar").style.padding = "50px 50px";
-    document.getElementById("logo").style.fontSize = "35px";
-    document.getElementById("navbar").style.backgroundColor="#f1f1f100";
+    // document.getElementById("mainNav").style.padding = "40px 30px";
+    document.getElementById("logo-navbar").style.width=250;
+    // document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("mainNav").style.backgroundColor="#021529c0";
 
   }
 }
+
+
+
 
 
 
